7f0f00ffe9dc1a2e18d8d6fd532968ba9aeb57d1
Daro <argiesdario@gmail.com>
2015-09-04 16:24:53 -0300

    Mas cambios chicos

--- src/dto/ClienteDto.java
+++ src/dto/ClienteDto.java
@@ -10,49 +10,18 @@ public class ClienteDto implements Serializable{
 	private String razonSocial;
 	private String mail;
 	private String CUIT;
-	private int unidades;
-	private int meses;
-	private int operaciones;
 	
 	
 	
-	public ClienteDto(String razonSocial, String mail, String CUIT, int unidades,
-			int meses, int operaciones) {
+	public ClienteDto(String razonSocial, String mail, String CUIT) {
 		super();
 		this.razonSocial = razonSocial;
 		this.mail = mail;
 		this.CUIT = CUIT;
-		this.unidades = unidades;
-		this.meses = meses;
-		this.operaciones = operaciones;
 	}
 	
 	public ClienteDto(){
 		
-	}
-	
-	public int getUnidades() {
-		return unidades;
-	}
-	
-	public void setUnidades(int unidades) {
-		this.unidades = unidades;
-	}
-	
-	public int getMeses() {
-		return meses;
-	}
-	
-	public void setMeses(int meses) {
-		this.meses = meses;
-	}
-	
-	public int getOperaciones() {
-		return operaciones;
-	}
-	
-	public void setOperaciones(int operaciones) {
-		this.operaciones = operaciones;
 	}
 	
 	public String getRazonSocial() {
