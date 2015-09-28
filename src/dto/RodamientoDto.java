dab332abebbb99e82d94847342a6aa84f0febb46
Martin <fmartinhuber@gmail.com>
2015-09-28 18:34:58 -0300

    aaa

--- src/dto/RodamientoDto.java
+++ src/dto/RodamientoDto.java
@@ -99,5 +99,44 @@ public class RodamientoDto implements Serializable{
 	public void setCaracteristica(String caracteristica) {
 		this.caracteristica = caracteristica;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+		result = prime * result + ((origen == null) ? 0 : origen.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RodamientoDto other = (RodamientoDto) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (marca == null) {
+			if (other.marca != null)
+				return false;
+		} else if (!marca.equals(other.marca))
+			return false;
+		if (origen == null) {
+			if (other.origen != null)
+				return false;
+		} else if (!origen.equals(other.origen))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
